41f9368347daad0afbd4807d4c608d9363541022
boss <boss@boss>
2025-01-17 15:03:03 +0530

    updated comments

--- 1.automated_backup.sh
+++ 1.automated_backup.sh
@@ -19,3 +19,14 @@ if [ $? -eq 0 ]; then
 else
 	echo "Backup failed!"
 fi
+
+# 1. Variables
+# SOURCE_DIR - Directory that contains the files you want to backup
+# DEST_DIR - Drectory where the backup file will be saved
+# BACKUP_NAME - Generates a unique backup filename based on the current date and time using the date command.
+
+# 2. Backup Creation
+# tar -czf "$DEST_DIR/$BACKUP_NAME" -C "$SOURCE_DIR" . - This command creates a tar.gz compressed archive of the contents of the SOURCE_DIR. The -C option makes tar change to the SOURCE_DIR before creating the archive.
+
+# 3. Error Handling
+# The if [ $? -eq 0 ] checks if the tar command was successful ($?) is the exit code of the last command). If successful, it prints a success message; otherwise, it prints an error message.
